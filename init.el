fc14bcb70afe79501908b26f6c6b4bc7ac6f7fa8
David Mebo <mebodave@gmail.com>
2023-01-10 14:18:45 +0100

    org bullets setup added

--- init.el
+++ init.el
@@ -69,6 +69,9 @@ If you experience freezing, decrease this.  If you experience stuttering, increa
 (require 'progs)
 
 (require 'typescript)
+
+(require 'org)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -76,7 +79,7 @@ If you experience freezing, decrease this.  If you experience stuttering, increa
  ;; If there is more than one, they won't work right.
  '(company-show-quick-access t nil nil "Customized with use-package company")
  '(package-selected-packages
-   '(flyspell-correct-ivy tide typescript-mode web-mode company-box company evil-nerd-commenter format-all highlight-indent-guides smartparens flycheck-popup-tip flycheck-posframe flycheck yasnippet-snippets yasnippet projectile undo-tree popup-kill-ring which-key disk-usage counsel amx ivy eglot doom-modeline all-the-icons doom-themes diminish auto-package-update use-package))
+   '(org-bullets flyspell-correct-ivy tide typescript-mode web-mode company-box company evil-nerd-commenter format-all highlight-indent-guides smartparens flycheck-popup-tip flycheck-posframe flycheck yasnippet-snippets yasnippet projectile undo-tree popup-kill-ring which-key disk-usage counsel amx ivy eglot doom-modeline all-the-icons doom-themes diminish auto-package-update use-package))
  '(warning-suppress-types '((comp) (comp) (comp) (comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
