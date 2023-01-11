397f2d3e40553e3e89f590fa61cf4765f8a855d8
David Mebo <mebodave@gmail.com>
2023-01-11 12:38:17 +0100

    elpy setup complete

--- elisp/org.el
+++ elisp/org.el
@@ -3,6 +3,14 @@
 ;;; code:
 
 ;; org
+;; (defun org-mode-setup ()
+;;  (variable-pitch-mode 1)
+;;  (visual-line-mode 1))
+
+(use-package org
+ ;; :hook (org-mode . org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾"))
 
 (provide 'org)
 ;;; org.el ends here
--- elisp/python.el
+++ elisp/python.el
@@ -8,6 +8,7 @@
   :defer t
   :init
   (advice-add 'python-mode :before 'elpy-enable)
+  (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
   (setq elpy-shell-echo-output nil)
   (setq elpy-rpc-python-command "python")
   (setq elpy-rpc-timeout 2)
@@ -23,6 +24,13 @@
 ;; Elpy is creating the RPC virtualenv (’/home/meistens/.emacs.d/elpy/rpc-venv’)
 ;; Automatically install the RPC dependencies from PyPI (needed for completion,
 ;; autoformatting and documentation) ?
+;; install patch if patch not found command
+;; back and forth, only 3.11.1 works... 3.5.2 won't work with openssl-devel
+;; 3.11.1 no compilation error with tk-devel installed
+;; pyenv virtualenv 3.5.2 my-project-virtualenv-name make sure virtualenv is
+;; installed
+;; update: not gonna bother setting up virtualenv right now
+
 ;; (y or n) y
 (provide 'python)
 ;;; python.el ends here
