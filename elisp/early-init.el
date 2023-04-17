b3f630aa9ac71a5dc1f2e2a802beffa9746aca2c
David Mebo <mebodave@gmail.com>
2023-04-17 23:46:57 +0100

    brief (and bad) summary of what file does

---
+++ elisp/early-init.el
@@ -0,0 +1,32 @@
+;;; early-init.el --- -*- lexical-binding: t -*-
+;;; commentary:
+;; Basically, these settings load up way before the other ones by default.
+;;; code:
+
+;; garbage collection
+(setq gc-cons-threshold 100000000)
+
+
+;; enable packages at startup
+(setq package-enable-at-startup nil)
+
+
+;; file name handler
+(defvar file-name-handler-alist-original file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+
+;; UnsetSRF
+(setq site-run-file nil)
+
+;; menu bar
+(menu-bar-mode -1)
+
+;; scroll bar
+(scroll-bar-mode -1)
+
+;; tool bar
+(tool-bar-mode 0)
+
+(provide 'early-init)
+;;; early-init.el ends here
