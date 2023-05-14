7d1ef4b45c484d6bddd22c6076dc7a5abbbd3446
David Mebo <mebodave@gmail.com>
2023-05-14 09:55:37 +0100

    moved keybinds to separate config file, changed sys-configs.el to sys-settings.el

--- elisp/sys-keybinds.el
+++ elisp/sys-keybinds.el
@@ -1,10 +1,25 @@
 ;;; sys-keybinds.el --- -*- lexical-binding: t -*-
 ;;; Commentary:
-;; File for all keybinds.  Make sure no conflicts with predefined keybinds
+;; Keybinds here. Makes it easier to keep track em
 ;;; Code:
+
+;; Unbind unneeded keys
+(global-set-key (kbd "M-z") nil) ;; zaps a character in a line
+
+(global-set-key (kbd "M-m") nil) ;; back-to-indentation
+
+(global-set-key (kbd "C-x C-z") nil) ;; suspend emacs, like a minimize but isn't really
+
+(global-set-key (kbd "M-/") nil) ;; dabbrev expand
+
+;; Truncate lines (rebound from downcase)
+(global-set-key (kbd "C-x C-l") #'toggle-truncate-lines)
 
 ;; liveserver start
 (global-set-key (kbd "C-c l") 'dm/start-impatient-server)
+
+;; ESC now in place of C-g
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 (provide 'sys-keybinds)
 ;;; sys-keybinds.el ends here
