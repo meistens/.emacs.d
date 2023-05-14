24b0338415f9150fe4022921f5f09eacbeb0b3c2
David Mebo <mebodave@gmail.com>
2023-05-14 12:24:11 +0100

    moved programming-related configs to eglot and auto-detect venv for python

--- elisp/sys-eglot.el
+++ elisp/sys-eglot.el
@@ -1,6 +1,6 @@
 ;;; sys-eglot.el --- -*- lexical-binding: t -*-
 ;;; commentary:
-;; server for C, Js and Ts.  To add more, swap out/use existing setup to
+;; server for C, Js, Py and Ts.  To add more, swap out/use existing setup to
 ;; configure yours
 ;;; code:
 
@@ -12,10 +12,25 @@
   (add-to-list 'eglot-server-programs '(c++-mode . ("clangd")))
   (add-to-list 'eglot-server-programs '(js-mode . ("typescript-language-server" "--stdio")))
   (add-to-list 'eglot-server-programs '(typescript-mode . ("typescript-language-server" "--stdio")))
+  (add-to-list 'eglot-server-programs '(python-mode . ("pyls")))
   (add-hook 'c-mode-hook 'eglot-ensure)
   (add-hook 'c++-mode-hook 'eglot-ensure)
   (add-hook 'js-mode-hook 'eglot-ensure)
-  (add-hook 'typescript-mode-hook 'eglot-ensure))
+  (add-hook 'typescript-mode-hook 'eglot-ensure)
+  (add-hook 'python-mode-hook 'eglot-ensure))
+
+;; Detect Venv automatically, not gonna go through the trouble of doing
+;; a separate setup...
+(defun eglot-detect-virtualenv ()
+  (let* ((venv-dir-name "env")
+	 (venv-bin-dir (locate-dominating-file default-directory venv-dir-name)))
+    (when venv-bin-dir
+      (let* ((python-executable (concat venv-bin-dir "bin/python"))
+	     (venv-name (file-name-base (directory-file-name venv-bin-dir))))
+	(setq-local eglot-python-command python-executable)
+	(message "Virtual environment activated: %s" venv-name)))))
+
+(add-hook 'python-mode-hook 'eglot-detect-virtualenv)
 
 (provide 'sys-eglot)
 ;;; sys-eglot.el ends here
