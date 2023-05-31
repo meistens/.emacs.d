0fbe25ca01b4f5bd49282fc52ff7b0ec4ebe4785
David Mebo <mebodave@gmail.com>
2023-05-31 22:14:42 +0100

    added file for custom-set-* and removed license

---
+++ custom.el
@@ -0,0 +1,21 @@
+;;; custom.el --- -*- lexical-binding: t -*-
+;;; commentary:
+;; File contains the custom-set stuff Emacs loves to dump in the init.el
+;;; code:
+
+(provide 'custom)
+;;; custom.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(pyvenv js2-mode typescript-mode impatient-mode rainbow-mode web-mode org-appear org-superstar kind-icon corfu evil-nerd-commenter format-all highlight-indent-guides smartparens flycheck-popup-tip flycheck-posframe flycheck yasnippet-snippets yasnippet projectile undo-tree popup-kill-ring which-key disk-usage nerd-icons-completion marginalia consult orderless vertico eglot ace-window command-log-mode doom-modeline doom-themes diminish auto-package-update use-package))
+ '(warning-suppress-types '((comp) (comp) (comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -7,55 +7,34 @@
 
 ;; early init
 (require 'early-init)
-
 ;; package manager
 (require 'package-manager)
-
 ;; UI
 (require 'sys-UI)
-
 ;; eglot
 (require 'sys-eglot)
-
 ;; global functionalities
 (require 'sys-globals)
-
 ;; some configs
 (require 'sys-settings)
-
-;; programming tools
-;; (require 'sys-progs)
-
 ;; org
 (require 'sys-org)
-
 ;; HTML, CSS, liveserver
 (require 'sys-web)
-
 ;; TypeScript
 (require 'sys-typescript)
-
 ;; JavaScript
 (require 'sys-javascript)
-
 ;; Shell (lisp shell, and related shell functions)
 (require 'sys-shell)
-
 ;; keybinds
 (load "sys-keybinds")
+;; Python
+(require 'sys-python)
+;; Custom stuff
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(direnv auto-virtualenv pipenv rg company-box company cape yasnippet-snippets which-key web-mode vertico use-package undo-tree typescript-mode smartparens rainbow-mode projectile popup-kill-ring org-superstar org-appear orderless nerd-icons-completion marginalia kind-icon js2-mode impatient-mode highlight-indent-guides format-all flycheck-posframe flycheck-popup-tip evil-nerd-commenter eglot doom-themes doom-modeline disk-usage diminish corfu consult command-log-mode auto-package-update all-the-icons ace-window)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
